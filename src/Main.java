0b24b3ad2175413dab2a1ddac9c87bbba80ecc7d
Cosby1992 <Andersbirkedahl@hotmail.com>
2018-09-05 12:45:24 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("Hello World!");
+        System.out.println("I'm back!");
+        System.out.println("I just made a bloody program");
+        System.out.println("You want some more of this bitch?");
+        System.out.println("I can keep this going ALL DAY MOTHERF*****!! \nCrazyyyy new line going on now!!");
+    }
+}
